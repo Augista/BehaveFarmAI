2a199ddc09f1ec8ade29c7e584a424d6ddb0a39b
Augista <augistapradana11@gmail.com>
2026-04-09 19:39:20 +0700

    fix: scrolling

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -75,10 +75,11 @@ export function Sidebar({
       {/* Sidebar */}
       <aside
         className={`
-          fixed top-0 left-0 h-screen w-64 bg-linear-to-b from-emerald-700 to-emerald-900 text-white
+          sticky overflow-y-auto top-0 h-screen w-64 
+          bg-linear-to-b from-emerald-700 to-emerald-900 text-white
           transform transition-transform duration-300 ease-in-out z-40
           ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
-          lg:static lg:translate-x-0 flex flex-col overflow-hidden
+          lg:translate-x-0 flex flex-col
         `}
       >
         {/* Header */}
